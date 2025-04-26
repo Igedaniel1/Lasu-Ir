398e57b04bebf4bf1d8c816726feb9e04641124c
Igedaniel1 <danielige80@gmail.com>
2025-04-26 19:52:10 +0100

    Update start.sh

--- dspace-jspui/start.sh
+++ dspace-jspui/start.sh
@@ -4,11 +4,10 @@
 catalina.sh start
 
 # Wait for Tomcat to finish starting and unpack the WAR
-# This can vary based on your app, but you can wait a few seconds
 sleep 15
 
-# Now move the test file into the ROOT directory after the unpacking process
-cp /usr/local/tomcat/test-assets/index.jsp /usr/local/tomcat/webapps/ROOT/index.jsp
+# Copy the test index.jsp into the ROOT directory (after WAR is unpacked)
+cp /usr/local/tomcat/webapps/ROOT/test-assets/index.jsp /usr/local/tomcat/webapps/ROOT/index.jsp
 
 # Finally, keep Tomcat running in the foreground
 catalina.sh run
